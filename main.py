476ede18a785ce56bbf047f7b7e1a857656a0ce2
ashtyngroves-12 <817376@mystma.org>
2022-09-21 14:48:29 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x,y):
+    if abs(10-x) < abs(10-y):
+         return y
+    elif abs(10-x) < abs(10-y):
+        return x
+    else: abs(x==y)
+    return 0
